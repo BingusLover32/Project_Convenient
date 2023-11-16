63b72fc9bf6b43d15c1fb7975d5eaec264189ddc
Olly <ollyender1@gmail.com>
2023-11-16 13:48:04 +0000

    Added pixel art playable character overhaul

---
+++ Assets/PlayerAnimationScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationScript : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        // Assuming the Animator component is attached to the same GameObject
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                animator.Play("PlayerURight");
+            }
+            else
+            {
+                animator.Play("PlayerUp");
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                animator.Play("PlayerURight");
+            }
+            else
+            {
+                animator.Play("PlayerRight");
+            };
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                animator.Play("PlayerDRight");
+            }
+            else
+            {
+                animator.Play("PlayerDown");
+            };
+        }
+
+    }
+}
